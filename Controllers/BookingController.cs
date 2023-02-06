28938f0ff5867f6989858120aa2b596598ebeba6
jkaylight <jekayinoluwa.olabemiwo@venturegardengroup.com>
2023-02-06 23:48:56 +0100

    setting up booking list endpoint

---
+++ Controllers/BookingController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Flyline.Data;
+using Flyline.ReadModels;
+
+namespace Flyline.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        private readonly Entities _entities;
+
+        public BookingController(Entities entities)
+        {
+            _entities = entities;
+        }
+
+
+        [HttpGet("{email}")]
+        [ProducesResponseType(500)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(typeof(IEnumerable<BookingRm>), 200)]
+        public ActionResult<IEnumerable<BookingRm>> List(string email)
+        {
+            // Go to the entities, inside the Flights dbase, transform the flight entries to an array
+            var bookings = _entities.Flights.ToArray()
+                .SelectMany(f => f.Bookings
+                    .Where(b => b.PassengerEmail == email)
+                    .Select(b => new BookingRm(
+                        f.Id,
+                        f.Airline,
+                        f.Price.ToString(),
+                        new TimePlaceRm(f.Arrival.Place, f.Arrival.Time),
+                        new TimePlaceRm(f.Departure.Place, f.Departure.Time),
+                        b.NumberOfSeats,
+                        email
+                        )));
+
+            return Ok(bookings);
+
+        }
+    }
+}
---
+++ ReadModels/BookingRm.cs
@@ -0,0 +1,12 @@
+﻿namespace Flyline.ReadModels
+{
+    public record BookingRm(
+        Guid FlighyIf,
+        string Airline,
+        string Price,
+        TimePlaceRm Arrival,
+        TimePlaceRm Departure,
+        int NumberOfBookedSeats,
+        string PassengerEmail
+        );
+}
